0b167a87d5388c9c3d09df25f5e98d3737f82f0f
Tobias Ouwejan <tobiasouwejan@gmail.com>
2018-06-08 10:28:30 +0200

    Add line-break-after directive

--- src/app/line-break.directive.ts
+++ src/app/line-break.directive.ts
@@ -2,14 +2,46 @@ import { Directive, ElementRef, Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Directive({
-    selector: '.line-break-before'
+    selector: '[.line-break-before, .line-break-after]'
 })
 export class LineBreakDirective {
     constructor(private elementRef: ElementRef, private renderer: Renderer2, @Inject(DOCUMENT) private document) {}
 
     ngOnInit() {
-        const child = document.createElement('div');
-        child.classList.add('line-break');
-        this.renderer.insertBefore(this.elementRef.nativeElement.parentNode, child, this.elementRef.nativeElement);
+        if (this.elementRef.nativeElement.classList.contains('line-break-before')) {
+            this.insertBefore();
+        }
+        if (this.elementRef.nativeElement.classList.contains('line-break-after')) {
+            this.insertAfter();
+        }
+    }
+
+    private insertBefore() {
+        const breakElement = this.createBreakElement();
+        this.renderer.insertBefore(
+            this.elementRef.nativeElement.parentNode,
+            breakElement,
+            this.elementRef.nativeElement
+        );
+    }
+
+    private insertAfter() {
+        const breakElement = this.createBreakElement();
+        this.renderer.insertBefore(
+            this.elementRef.nativeElement.parentNode,
+            breakElement,
+            this.elementRef.nativeElement.nextSibling
+        );
+        // this.renderer.insertBefore(
+        //     this.elementRef.nativeElement.parentNode,
+        //     breakElement,
+        //     this.elementRef.nativeElement
+        // );
+    }
+
+    private createBreakElement(): Element {
+        const breakElement = document.createElement('div');
+        breakElement.classList.add('line-break');
+        return breakElement;
     }
 }
